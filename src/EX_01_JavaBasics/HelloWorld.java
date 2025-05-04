a3454d14ca88d960d767192fbfb1b90d3c688f6d
Shaik Mishad <shaik.mishad@ibm.com>
2025-05-04 11:30:43 +0530

    Initial commit

---
+++ src/EX_01_JavaBasics/HelloWorld.java
@@ -0,0 +1,11 @@
+package EX_01_JavaBasics;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Pramod");
+    }
+
+}
