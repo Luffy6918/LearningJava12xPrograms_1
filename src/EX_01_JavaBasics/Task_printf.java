2b2134a9bb7764f1cdad2fdf7c14fd9a2543cdf8
ShaikMishad <shaikmishd083@gmail.com>
2025-05-16 10:55:01 +0530

    git commit Lab002

---
+++ src/EX_01_JavaBasics/Task_printf.java
@@ -0,0 +1,26 @@
+package EX_01_JavaBasics;
+
+public class Task_printf {
+
+    public static void main(String[] args) {
+        int i =5;
+
+        System.out.printf("table of 5*1 is %d",i);
+        System.out.println();
+        System.out.printf("table of 5*2 is %d",i*2);
+        System.out.println();
+        System.out.printf("table of 5*3 is %d",i*3);
+        System.out.println();
+        System.out.printf("table of 5*4 is %d",i*4);
+        System.out.println();
+        System.out.printf("table of 5*5 is %d",i*5);
+        System.out.println();
+        System.out.printf("table of 5*6 is %d",i*6);
+        System.out.println();
+        System.out.printf("table of 5*7 is %d",i*7);
+        System.out.println();
+        System.out.printf("table of 5*8 is %d",i*8);
+        System.out.println();
+
+    }
+}
