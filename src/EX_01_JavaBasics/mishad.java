b9e1b6fa35f16bc644493c22f128181311f0ba78
ShaikMishad <shaikmishd083@gmail.com>
2025-05-16 10:13:40 +0530

    git commit Lab002

---
+++ src/EX_01_JavaBasics/mishad.java
@@ -0,0 +1,19 @@
+package EX_01_JavaBasics;
+
+public class mishad {
+    public static void main(String[] args) {
+        int age = 10;
+
+        System.out.println(age);
+    }
+
+        public static void main ( int args){
+
+            int age = 20;
+
+            System.out.println(age);
+
+
+        }
+
+}
