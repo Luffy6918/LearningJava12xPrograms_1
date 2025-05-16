8a0087918c5077d7542a5867572e8e9888269d1c
ShaikMishad <shaikmishd083@gmail.com>
2025-05-16 10:45:22 +0530

    git commit Lab002

---
+++ src/EX_01_JavaBasics/Printf.java
@@ -0,0 +1,15 @@
+package EX_01_JavaBasics;
+
+public class Printf {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.printf("age is %d",a);
+        System.out.printf("value of b is %d",b);
+
+
+
+
+    }
+}
