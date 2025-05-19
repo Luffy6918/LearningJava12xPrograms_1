7302593c5ff5841a81599b0e1ab44f92d2fcb4c7
ShaikMishad <shaikmishd083@gmail.com>
2025-05-19 10:05:41 +0530

    git commit Lab002

---
+++ src/Java_Operators/Operators_InterviewQues.java
@@ -0,0 +1,20 @@
+package Java_Operators;
+
+public class Operators_InterviewQues {
+    //Concatination and addition
+
+    public static void main(String[] args) {
+
+        String a="Mishad";
+        String b="Shaik";
+
+        int c=10;
+        int d=20;
+        System.out.println(a+b+c+d);
+        System.out.println(a+b+(c+d));
+        System.out.println(c+d+a+b);
+
+    }
+
+
+}
