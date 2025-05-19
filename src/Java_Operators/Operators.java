ac3836bca8c157dee7a9d1fd4b2be9daa33bb29d
ShaikMishad <shaikmishd083@gmail.com>
2025-05-19 09:54:12 +0530

    git commit Lab002

---
+++ src/Java_Operators/Operators.java
@@ -0,0 +1,26 @@
+package Java_Operators;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        boolean c =a>b;
+
+        System.out.println(c);
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        boolean d= a>b;
+        boolean e=a<b;
+        System.out.println(d||e);
+        System.out.println(d&&e);
+        System.out.println(d==e);
+
+
+
+    }
+}
