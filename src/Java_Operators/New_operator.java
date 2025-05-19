9d7a4cd6951cdf2fcb7afced6847d13f96eec06e
ShaikMishad <shaikmishd083@gmail.com>
2025-05-19 10:22:44 +0530

    git commit Lab002

---
+++ src/Java_Operators/New_operator.java
@@ -0,0 +1,9 @@
+package Java_Operators;
+
+public class New_operator {
+    public static void main(String[] args) {
+        String a = new String("Mishad");
+        System.out.println(a);
+        System.out.println(a instanceof String);
+    }
+}
