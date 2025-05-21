68c0ae90e2838a5bd4100f7ed3419e087c97dd52
ShaikMishad <shaikmishd083@gmail.com>
2025-05-21 09:27:18 +0530

    git commit Lab002

---
+++ src/Java_Operators/Type_casting.java
@@ -0,0 +1,9 @@
+package Java_Operators;
+
+public class Type_casting {
+    public static void main(String[] args) {
+        int a=300;
+        byte b= (byte)a;
+        System.out.println(b);
+    }
+}
