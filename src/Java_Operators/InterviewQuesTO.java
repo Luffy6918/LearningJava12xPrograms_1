dd739e3c9ac4248642be27a01cdc9144d950c861
ShaikMishad <shaikmishd083@gmail.com>
2025-05-21 10:28:10 +0530

    git commit Lab002

---
+++ src/Java_Operators/InterviewQuesTO.java
@@ -0,0 +1,13 @@
+package Java_Operators;
+
+public class InterviewQuesTO {
+
+    public static void main(String[] args) {
+        //print the max value using Terenary Opetrator
+
+        int a=40;
+        int b=26;
+        int max= a>b?a:b;
+        System.out.println(max);
+    }
+}
